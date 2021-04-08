97533e88bafc2b9c52c4cc87e7455c65a17aeb6c
Ihor Rybak <jrybak23@gmail.com>
2021-04-08 21:48:08 +0300

    fix: budget, font-awesome icons

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
+import {HomeRoutingModule} from './home-routing.module';
+import {HomeComponent} from './home.component';
 import {ExcelReaderService} from '../excel-reader/excel-reader.service';
 import {FormsModule} from '@angular/forms';
-
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { faFileDownload, faFolder } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -14,10 +15,15 @@ import {FormsModule} from '@angular/forms';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [
     ExcelReaderService
   ]
 })
-export class HomeModule { }
+export class HomeModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faFolder, faFileDownload);
+  }
+}
